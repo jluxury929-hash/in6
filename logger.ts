a8563527b7a78485411af3c7a6eb984c878af27f
Josh Platt <jluxury929@gmail.com>
2025-12-07 13:03:43 -0500

    Create logger.ts

---
+++ logger.ts
@@ -0,0 +1,15 @@
+// Simple Logger Utility Placeholder
+import winston from 'winston';
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(info => `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`)
+    ),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
+
+export default logger;
